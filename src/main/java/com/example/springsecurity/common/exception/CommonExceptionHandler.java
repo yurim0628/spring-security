12f1d7bfff8e91602eebc5031866ffd02b44d09b
yurim0628 <dbfla0303@naver.com>
2024-03-07 23:36:51 +0900

    로그인 관련 예외 코드 추가

--- src/main/java/com/example/springsecurity/common/exception/CommonExceptionHandler.java
+++ src/main/java/com/example/springsecurity/common/exception/CommonExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class CommonExceptionHandler {
+
     @ExceptionHandler(value = CustomException.class)
     public ResponseEntity<Response<Void>> handleCustomException(
             CustomException e,
@@ -19,11 +20,13 @@ public class CommonExceptionHandler {
         HttpStatus status = e.getErrorCode().getStatus();
         String message = e.getErrorCode().getMessage();
         String url = request.getRequestURI();
+
         log.error("Custom Exception - status: {}, message: {}, url: {}",
                 status,
                 message,
                 url
         );
+
         return new ResponseEntity<>(
                 Response.fail(message),
                 status
--- src/main/java/com/example/springsecurity/common/exception/ErrorCode.java
+++ src/main/java/com/example/springsecurity/common/exception/ErrorCode.java
@@ -4,12 +4,18 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
 
+    JSON_PROCESSING_ERROR(BAD_REQUEST, "JSON 처리 중 오류가 발생했습니다."),
+    INVALID_REQUEST(BAD_REQUEST, "요청 형식이 올바르지 않습니다."),
+    INVALID_CREDENTIALS(UNAUTHORIZED, "아이디 또는 비밀번호를 확인해주세요."),
+    INTERNAL_AUTHENTICATION_ERROR(INTERNAL_SERVER_ERROR, "로그인 중에 내부 서버 오류가 발생했습니다."),
+    UNKNOWN_ERROR(INTERNAL_SERVER_ERROR, "로그인 중에 알 수 없는 오류가 발생했습니다."),
     EMAIL_ALREADY_EXISTS(CONFLICT, "이미 사용 중인 이메일 입니다.");
 
     private final HttpStatus status;
